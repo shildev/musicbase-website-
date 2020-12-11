a5d1fe0596bc571c065bf9e974e2c3eff613929a
shildev <ssivas6@hotmail.com>
2020-12-11 18:46:44 +0000

    added sendgrid production configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://shilsmusicbase.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+    ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
